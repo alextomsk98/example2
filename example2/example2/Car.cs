46a9db9d95ec2e1b5fe36420af0dde43b7eaad0f
alextomsk98 <shavlo.2016@mail.ru>
2018-02-05 17:55:02 +0700

    laba1(2)
    practica

---
+++ example2/example2/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example2
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPN.", petName, currSpeed);
+        }
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
